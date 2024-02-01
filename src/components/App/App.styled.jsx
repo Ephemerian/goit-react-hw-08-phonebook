642681393bd76d9208287342fbaf8c4166f4d041
Ephemerian <106543055+Ephemerian@users.noreply.github.com>
2024-02-01 03:25:07 +0200

    Initial commit

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+export const Wrapper = styled.div`
+    width: 1280px;
+    margin: 0 auto;
+`
+export const H1 = styled.h1`
+    text-align: center;
+    color: #067ad0;
+`
+export const H2 = styled.h2`
+    text-align: center;
+    color: #067ad0;
+    font-size: 32px;
+`
